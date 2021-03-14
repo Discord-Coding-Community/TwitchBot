316351eecc3411242bddd24bca0f96b1659ce284
Nimbi <Darkwater409@gmail.com>
2021-03-14 11:48:24 -0700

    03/14/2021
     - Update 'announce.js'

--- commands/mod/announce.js
+++ commands/mod/announce.js
@@ -26,7 +26,7 @@ module.exports = class newsCommand extends Command {
 
     run(msg, { text }) {
         msg.guild.channels.cache
-        let embed = new Discord.RichEmbed();
+        let embed = new Discord.Embed();
         let ch = msg.mentions.channels.first()
             .setAuthor('Twitchbot', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
             .setFooter('Twitchbot | twitchbot.newhorizon.dev', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
