b8487528a358a9e0efc09bb026d7d89cf7cfe3d1
Nimbi <Darkwater409@gmail.com>
2021-03-15 07:10:27 -0700

    03/15/2021

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
 
-PREFIX = process.env.CLIENT_PREFIX
-ACTIVITY = process.env.CLIENT_ACTIVITY
-
 module.exports = async(client) => {
 
-    console.log(`I am preparing to start....`);
-    console.log(`I am ready! Logged in as ${client.user.tag}!`);
-
-    client.user.setActivity(`${ACTIVITY} in ${client.guilds.cache.size} servers`)
+    client.user.setActivity(`help in ${client.guilds.cache.size} servers`)
         .then(console.log(`${client.user.tag} has started in ${client.guilds.cache.size} servers`))
         .catch(err => {
             console.error(err);
