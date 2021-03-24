ce786d5d89f507b61f2cb813ecd06fa8eb5007e7
Nimbi <Darkwater409@gmail.com>
2021-03-24 03:48:25 -0700

    03/24/2021
     - Update index.js
     - Update config.json

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { CommandoClient } = require('discord.js-commando');
+const Discord = require("discord.js");
 const AutoPoster = require('topgg-autoposter');
 const { Structures } = require('discord.js');
 const config = require('./TwitchBot/config/config.json');
 const path = require('path');
 const db = require('quick.db');
 const Topgg = require("@top-gg/sdk");
-const express = require("express");
 const Canvas = require('canvas');
-
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
 
 Structures.extend('Guild', function(Guild) {
     class MusicGuild extends Guild {
@@ -44,29 +45,14 @@ Structures.extend('Guild', function(Guild) {
 
 const TwitchBot = new CommandoClient({
     commandPrefix: config.prefix,
-    owner: config.discord_owner_id,
-    disableEveryone: true,
-    unknownCommandResponse: false
+    owner: config.discord_owner_id
 });
 
-const Discord = require("discord.js");
-const SQLite = require("better-sqlite3");
-const sql = new SQLite('./scores.sqlite');
-const app = express();
-const ap = AutoPoster('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyNzkzMDQzNzk5NzE2NjY1MyIsImJvdCI6dHJ1ZSwiaWF0IjoxNjE1OTg5NjY5fQ.AEcz-EVuU8-IQg2ZnDjhLGzn-w63S_kRfdH_m5hImVU', TwitchBot)
+const ap = AutoPoster(config.ap_api, TwitchBot)
 
 ap.on('posted', () => {
     console.log('Posted stats to Top.gg!')
 })
-
-const webhook = new Topgg.Webhook("azd5fg12w2d52cg1");
-
-app.post("/dblwebhook", webhook.middleware(), (req, res) => {
-
-    console.log(req.vote.user);
-});
-
-app.listen(27901);
 
 TwitchBot.once('ready', () => {
     console.log(TwitchBot.user.tag + ' ready.');
@@ -86,6 +72,78 @@ TwitchBot.once('ready', () => {
   TwitchBot.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
   TwitchBot.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
     console.log(TwitchBot.user.tag + ' has started in ' + TwitchBot.guilds.cache.size + ' servers')
+});
+
+TwitchBot.on("message", message => {
+  if (message.author.bot) return;
+  let score;
+  if (message.guild) {
+    score = TwitchBot.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    if(score.level < curLevel) {
+      score.level++;
+      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+    }
+    TwitchBot.setScore.run(score);
+  }
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+    
+    if(command === "points") {
+  return message.reply(`You currently have ${score.points} points and are level ${score.level}!`);
+}
+    
+    if(command === "give") {
+  // Limited to guild owner - adjust to your own preference!
+  if(!message.author.id === message.guild.owner) return message.reply("You're not the boss of me, you can't do that!");
+
+  const user = message.mentions.users.first() || TwitchBot.users.get(args[0]);
+  if(!user) return message.reply("You must mention someone or give their ID!");
+
+  const pointsToAdd = parseInt(args[1], 10);
+  if(!pointsToAdd) return message.reply("You didn't tell me how many points to give...")
+
+  // Get their current points.
+  let userscore = TwitchBot.getScore.get(user.id, message.guild.id);
+  // It's possible to give points to a user we haven't seen, so we need to initiate defaults here too!
+  if (!userscore) {
+    userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0, level: 1 }
+  }
+  userscore.points += pointsToAdd;
+
+  // We also want to update their level (but we won't notify them if it changes)
+  let userLevel = Math.floor(0.1 * Math.sqrt(score.points));
+  userscore.level = userLevel;
+
+  // And we save it!
+  TwitchBot.setScore.run(userscore);
+
+  return message.channel.send(`${user.tag} has received ${pointsToAdd} points and now stands at ${userscore.points} points.`);
+}
+
+if(command === "leaderboard") {
+  const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(message.guild.id);
+
+    // Now shake it and show it! (as a nice embed, too!)
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Leaderboard")
+    .setAuthor(TwitchBot.user.username, TwitchBot.user.avatarURL)
+    .setDescription("Our top 10 points leaders!")
+    .setColor(0x00AE86);
+
+  for(const data of top10) {
+    embed.addField(TwitchBot.users.cache.get(data.user).tag, `${data.points} points (level ${data.level})`);
+  }
+  return message.channel.send({embed});
+}
+
+  
 });
 
 TwitchBot.on('voiceStateUpdate', async(___, newState) => {
@@ -112,27 +170,6 @@ TwitchBot.on('voiceStateUpdate', async(___, newState) => {
 const guildNames = TwitchBot.guilds.cache.map(g => g.name).join("\n")
 const tags = TwitchBot.users.cache.map(u=> `${u.username}#${u.discriminator}`).join(", ");
 
-TwitchBot.on("message", message => {
-  if (message.author.bot) return;
-  let score;
-  if (message.guild) {
-    score = TwitchBot.getScore.get(message.author.id, message.guild.id);
-    if (!score) {
-      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
-    }
-    score.points++;
-    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
-    if(score.level < curLevel) {
-      score.level++;
-      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
-    }
-    TwitchBot.setScore.run(score);
-  }
-  if (message.content.indexOf(config.prefix) !== 0) return;
-
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-});
 
 TwitchBot.registry
     .registerDefaultTypes()
@@ -145,10 +182,10 @@ TwitchBot.registry
     ])
     .registerDefaultGroups()
     .registerDefaultCommands({
-        help: false,
-        eval: false,
-        ping: false,
-        prefix: false
+    help: false,
+    eval: false,
+    prefix: false,
+    unknownCommand: false
     })
     .registerCommandsIn(path.join(__dirname, 'TwitchBot/commands'));
 
@@ -157,5 +194,14 @@ console.log(Guilds, 'Connected!');
 Canvas.registerFont('./TwitchBot/resources/welcome/OpenSans-Light.ttf', {
     family: 'Open Sans Light'
 });
+
+TwitchBot.on("error", function(error){
+    console.error(`error: ${error}`);
+});
+
+TwitchBot.on("warn", function(info){
+    console.log(`warn: ${info}`);
+});
+
 
 TwitchBot.login(config.token);
