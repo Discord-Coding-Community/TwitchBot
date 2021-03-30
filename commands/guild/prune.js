570cae9b6de66a6411667d9488bcd2aa85854b8a
Nimbi <Darkwater409@gmail.com>
2021-03-30 05:12:30 -0700

    Update prune.js

--- commands/guild/prune.js
+++ commands/guild/prune.js
@@ -1,35 +1,73 @@
+const Discord = require('discord.js');
 const { Command } = require('discord.js-commando');
 
 module.exports = class PruneCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'prune',
-      aliases: ['delete-messages', 'bulk-delete', 'purge', 'clear'],
-      description: 'Delete up to 99 recent messages.',
-      group: 'guild',
-      memberName: 'prune',
-      guildOnly: true,
-      userPermissions: ['MANAGE_CHANNELS', 'MANAGE_MESSAGES'],
-      args: [
-        {
-          key: 'deleteCount',
-          prompt: 'How many messages do you want to delete?',
-          type: 'integer',
-          validate: deleteCount => deleteCount < 100 && deleteCount > 0
-        }
-      ]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'prune',
+            aliases: ['purge', 'delete', 'clean'],
+            group: 'guild',
+            memberName: 'prune',
+            description: 'Delete some messages from a Text Channel.',
+            examples: ['purge 5'],
+            guildOnly: true,
+            throttling: {
+                usages: 1,
+                duration: 3
+            },
+            clientPermissions: ['MANAGE_MESSAGES'],
+            userPermissions: ['MANAGE_MESSAGES'],
 
-  run(message, { deleteCount }) {
-    message.channel
-      .bulkDelete(deleteCount)
-      .then(messages => message.reply(`Deleted ${messages.size} messages`))
-      .catch(e => {
-        console.error(e);
-        return message.reply(
-          ':x: Something went wrong when trying to delete messages!'
-        );
-      });
-  }
+            args: [{
+                key: 'amount',
+                label: 'Number',
+                prompt: 'Please input a number between 0 and 100.',
+                type: 'integer'
+            }]
+        });
+    }
+
+
+    async run(message, args) {
+        const { channel } = message
+        const amount = Number(args.amount)
+        if (Number.isNaN(amount)) {
+            return message.reply(
+                '```css\n[ERROR] Please provide a valid number.\n```'
+            )
+        }
+
+        if (amount <= 0 || amount > 100) {
+            return message.reply(
+                '```css\n[ERROR] You need to input a number between 0 and 100.\n```'
+            )
+        }
+
+        const limit = args.amount + 1
+        try {
+            const messages = await channel.messages.fetch({ limit })
+            const deletedMessages = await channel.bulkDelete(messages)
+            const botMessages = deletedMessages.filter(m => m.author.bot)
+            const userPins = deletedMessages.filter(m => m.pinned)
+            const userMessages = deletedMessages.filter(m => !m.author.bot)
+            var embed = new Discord.MessageEmbed()
+                .setAuthor('Twitchbot', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
+                .setTitle("Prune Command Issued")
+                .setDescription('The following messages have been deleted.')
+                .setColor('RANDOM')
+                .setFooter('Twitchbot', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
+                .setThumbnail('https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
+                .setTimestamp(new Date().toISOString())
+                .addField("Bot Messages Deleted", botMessages.size, false)
+                .addField("User Pins Deleted", userPins.size, false)
+                .addField("User Messages Deleted", userMessages.size, false)
+                .addField("Total Messages Deleted", deletedMessages.size, false);
+            channel.send(embed)
+        } catch (err) {
+            console.error(err)
+            channel.send(
+                '```css\n[ERROR] ' + err.code + ': [' + err.message + ']\n```'
+            )
+        }
+    }
 };
