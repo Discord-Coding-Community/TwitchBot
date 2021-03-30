7a7091163d6ba83f4a70ec7b195a095f08bb3371
Nimbi <Darkwater409@gmail.com>
2021-03-30 06:03:49 -0700

    Update economy-commands.js

--- commands/other/economy-commands.js
+++ commands/other/economy-commands.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { Command } = require('discord.js-commando');
+const config = require('../../config.json');
 
 module.exports = class EconomyCommandsCommand extends Command {
     constructor(client) {
