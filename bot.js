88f47955372084c2dd426e248038788567e822cc
Nimbi <Darkwater409@gmail.com>
2021-04-13 13:04:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ Structures.extend('Guild', function(Guild) {
     return MusicGuild;
 });
 
-const Client = new Discord.client({ shardCount: 'auto' });
+const manager = new Discord.client({ shardCount: 'auto' });
 
 const client = new CommandoClient({
     commandPrefix: config.prefix,
