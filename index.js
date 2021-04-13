7470208d9683215f2b4272c2490fd320e514bc05
Nimbi <Darkwater409@gmail.com>
2021-04-13 09:16:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ manager.spawn(5);
 
 manager.on('shardCreate', (shard) => console.log('Launching Shard ' + shard.id));
 
-manager.on('ready', () => {
+manager.on('ready', (shard) => {
     const URL = 'https://api.discordextremelist.xyz/v2/bot/' + config.applicationID + '/stats';
 
     const reqHeaders = {
@@ -25,7 +25,7 @@ manager.on('ready', () => {
     }
 
     const reqBody = {
-        "guildCount": manager.shard.fetchClientValues('guilds.cache.size')
+        "guildCount": shard.fetchClientValues('guilds.cache.size')
     }
 
     fetch(URL, { method: "POST", headers: reqHeaders, body: JSON.stringify(reqBody) })
@@ -44,12 +44,8 @@ manager.on('ready', () => {
     })
 });
 
-const getServer = async(guildID) => {
-    const req = await manager.shard.broadcastEval(this.guilds.get + (guildID));
-
-    return (req.find((res) => !!res) || false);
-};
-
 manager.on('message', (shard, message) => {
+
     console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
+
 });
