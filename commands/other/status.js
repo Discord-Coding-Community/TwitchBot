5dc457456af4b78de665e07dc073cca7841aaf77
Nimbi <Darkwater409@gmail.com>
2021-04-12 22:49:05 -0700

    Update status.js

--- commands/other/status.js
+++ commands/other/status.js
@@ -36,6 +36,9 @@ module.exports = class AboutCommand extends Command {
         let embed = new MessageEmbed()
             .setTitle(this.client.user.username)
             .setDescription(finalString)
+            .setColor('RANDOM')
+            .setTimestamp(new Date().toISOString())
+            .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
         message.channel.send(embed);
         return;
     } catch (e) {
