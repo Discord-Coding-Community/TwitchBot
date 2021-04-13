572206ec18cab097dfd874942ad29d2089227371
Nimbi <Darkwater409@gmail.com>
2021-04-12 22:08:11 -0700

    Update about.js

--- commands/other/about.js
+++ commands/other/about.js
@@ -19,27 +19,30 @@ module.exports = class AboutCommand extends Command {
     }
 
     run(message) {
-        const promises = [];
 
-        return Promise.all(promises)
-            .then(results => {
-                const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-                const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-                const totalChannels = results[2].reduce((acc, channelCount) => acc + channelCount, 0);
-                let embed = new MessageEmbed()
-                    .setTitle('TwitchBot')
-                    .setDescription('Twitch Integration bot built with `Discord.JS-Commando` and Twitch API.')
-                    .addField('Users', totalMembers, true)
-                    .addField('Channels', totalGuilds, true)
-                    .addField('Guilds', totalChannels, true)
-                    .addField('Prefix', config.prefix, true)
-                    .addField('Owners', config.owner_tag_1 + ',\n' + config.owner_tag_2, true)
-                    .addField('Github', '[' + config.github_team_name + '](https://github.com/' + config.github_team + '/' + config.github_repo + ')', true)
-                    .setThumbnail(this.client.user.displayAvatarURL())
-                    .setColor('RANDOM')
-                    .setTimestamp(new Date().toISOString())
-                    .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
-                message.channel.send(embed)
-            })
-    }
+        const guildCacheMap = this.client.guilds.cache;
+        const guildCacheArray = Array.from(guildCacheMap, ([name, value]) => ({
+            name,
+            value
+        }));
+        let memberCount = 0;
+        for (let i = 0; i < guildCacheArray.length; i++) {
+            memberCount = memberCount + guildCacheArray[i].value.memberCount;
+        }
+
+        let embed = new MessageEmbed()
+            .setTitle('TwitchBot')
+            .setDescription('Twitch Integration bot built with `Discord.JS-Commando` and Twitch API.')
+            .addField('Users', memberCount, true)
+            .addField('Channels', this.client.channels.cache.size, true)
+            .addField('Guilds', this.client.guilds.cache.size, true)
+            .addField('Prefix', config.prefix, true)
+            .addField('Owners', config.owner_tag_1 + ',\n' + config.owner_tag_2, true)
+            .addField('Github', '[' + config.github_team_name + '](https://github.com/' + config.github_team + '/' + config.github_repo + ')', true)
+            .setThumbnail(this.client.user.displayAvatarURL())
+            .setColor('RANDOM')
+            .setTimestamp(new Date().toISOString())
+            .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
+        message.channel.send(embed)
+    };
 };
