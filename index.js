2b447f0407abde5569e8e8fc42d4e5798e38f740
Nimbi <Darkwater409@gmail.com>
2021-04-13 14:10:54 -0700

    Update ShardManager

--- index.js
+++ index.js
@@ -13,41 +13,4 @@ const manager = new ShardingManager('./bot.js', {
 
 manager.on('shardCreate', (shard) => console.log('Launching Shard: ' + shard.id));
 
-manager.on('connect', async(shard) => {
-
-    const URL = 'https://api.discordextremelist.xyz/v2/bot/' + config.applicationID + '/stats';
-
-    const reqHeaders = {
-        "Content-Type": "application/json",
-        "Authorization": config.delAPI
-    }
-
-    const reqBody = {
-        "guildCount": shard.fetchClientValues('guilds.cache.size')
-    }
-
-    fetch(URL, { method: "POST", headers: reqHeaders, body: JSON.stringify(reqBody) })
-        .then((res) => {
-            return res.json()
-        })
-        .then((json) => {
-            console.log(json)
-        })
-
-    shard.broadcastEval(`
-        (async () => {
-            const srvCount = await shard.fetchClientValues('guilds.cache.size');
-            const usrCount = await shard.broadcastEval('this.guilds.cache.map((guild) => guild.members.cache.size)');
-            let channel = await this.channels.cache.get('831627743120588891')
-            if (channel) {
-                let MessageEmbed = require('discord.js');
-                let embed = new MessageEmbed()
-                .setTitle('**_Shard Status_**')
-                .setDescription('▫️ **Total Guilds**: ${srvCount.reduce((p, n) => p + n, 0)}｜▫️ **Total Users**: ${usrCount}')
-                .setColor('RANDOM')
-                channel.send({ embed: ${JSON.stringify(embed)} });
-            }
-        })();
-    `);
-});
 manager.spawn('auto');
