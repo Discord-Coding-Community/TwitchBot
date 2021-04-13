cb1e2432d0646d11f2d3074cfa8abfa86824c0ef
Nimbi <Darkwater409@gmail.com>
2021-04-13 08:12:38 -0700

    Update DEL API Code

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ const getServer = async(guildID) => {
     return (req.find((res) => !!res) || false);
 };
 
+manager.on('ready', () => {
+    const fetch = require("node-fetch");
+    const URL = 'https://api.discordextremelist.xyz/v2/bot/' + config.applicationID + '/stats';
+
+    const reqHeaders = {
+        "Content-Type": "application/json",
+        "Authorization": config.delAPI
+    }
+
+    const reqBody = {
+        "guildCount": client.guilds.size
+    }
+
+    fetch(URL, { method: "POST", headers: reqHeaders, body: JSON.stringify(reqBody) })
+        .then((res) => {
+            return res.json()
+        })
+        .then((json) => {
+            console.log(json);
+        })
+});
+
 manager.on('message', (shard, message) => {
     console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
 });
