5ab97da2fd49b037931e5442057416546a76efb5
Nimbi <Darkwater409@gmail.com>
2021-04-13 12:36:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,26 +13,21 @@ const manager = new ShardingManager('./bot.js', {
 
 manager.spawn(10);
 
-manager.on('shardCreate', (shard) => console.log('Launching Shard: ' + shard.id));
+const URL = 'https://api.discordextremelist.xyz/v2/bot/' + config.applicationID + '/stats';
 
-manager.on('connect', (shard) => {
+const reqHeaders = {
+    "Content-Type": "application/json",
+    "Authorization": config.delAPI
+}
 
-    const URL = 'https://api.discordextremelist.xyz/v2/bot/' + config.applicationID + '/stats';
+const reqBody = {
+    "guildCount": manager.shard.fetchClientValues('guilds.cache.size')
+}
 
-    const reqHeaders = {
-        "Content-Type": "application/json",
-        "Authorization": config.delAPI
-    }
-
-    const reqBody = {
-        "guildCount": shard.fetchClientValues('guilds.cache.size')
-    }
-
-    fetch(URL, { method: "POST", headers: reqHeaders, body: JSON.stringify(reqBody) })
-        .then((res) => {
-            return res.json()
-        })
-        .then((json) => {
-            console.log(json);
-        })
-});
+manager.on('shardCreate', (shard) => fetch(URL, { method: "POST", headers: reqHeaders, body: JSON.stringify(reqBody) })
+    .then((res) => {
+        return res.json()
+    })
+    .then((json) => {
+        console.log(json);
+    }), console.log('Launching Shard: ' + shard.id));
