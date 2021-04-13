5e5c1e84c6de7416013432bdc8e6b1e4e5b0ae72
Nimbi <Darkwater409@gmail.com>
2021-04-13 14:26:15 -0700

    Create status.js

---
+++ commands/other/status.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require('discord.js');
+const { Command } = require('discord.js-commando');
+const config = require('../../config.json');
+
+module.exports = class StatusCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'status',
+            aliases: [
+                'shard-status',
+                'shard-info',
+                'si',
+                'ss'
+            ],
+            memberName: 'status',
+            group: 'other',
+            description: "Displays the shard status.",
+            examples: [
+                config.prefix + 'status'
+            ]
+        });
+    }
+
+    run(message, client) {
+        let s = client.shard;
+        let embed = new MessageEmbed()
+            .setDescription('**__Shard Status__**')
+            .addField('Total Users', `${s.fetchClientValues('userss.cache.size')}`, true)
+            .addField('Total Channels', `${s.fetchClientValues('channels.cache.size')}`, true)
+            .addField('Total Guilds', `${s.fetchClientValues('guilds.cache.size')}`, true)
+            .setThumbnail(this.client.user.displayAvatarURL())
+            .setColor('RANDOM')
+            .setTimestamp(new Date().toISOString())
+            .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
+        message.channel.send(embed)
+    } catch (e) {
+        console.error(e)
+        channel.send(
+            '```css\n[ERROR] ' + err.code + ': [' + err.message + ']\n```'
+        )
+    }
+};
