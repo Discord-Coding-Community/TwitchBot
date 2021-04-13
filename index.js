20d34f5c06c2ae925273042194bc851c27bedc60
Nimbi <Darkwater409@gmail.com>
2021-04-13 09:03:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const manager = new ShardingManager('./bot.js', {
 
 });
 
-manager.spawn();
+manager.spawn(5);
 
 manager.on('shardCreate', (shard) => console.log('Launching Shard ' + shard.id));
 
