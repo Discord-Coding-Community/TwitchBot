1d113a5f0ff2d5283b77dcb6a559ad8a910d9199
Nimbi <Darkwater409@gmail.com>
2021-04-13 17:36:02 -0700

    Update say.js

--- commands/other/say.js
+++ commands/other/say.js
@@ -8,60 +8,32 @@ module.exports = class SayCommand extends Command {
             aliases: ['make-me-say', 'print'],
             memberName: 'say',
             group: 'other',
-            userPermissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
             clientPermissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
             description: 'Make the bot say anything!',
             args: [{
-                    key: 'announcementChannel',
-                    prompt: 'In which channel do you want the announcement to be sent?',
-                    type: 'string',
-                    default: message => message.channel.id
-                },
-                {
-                    key: 'text',
-                    prompt: ':microphone2: What do you want the bot to say?',
-                    type: 'string'
-                }
-            ]
+                key: 'text',
+                prompt: ':microphone2: What do you want the bot to say?',
+                type: 'string'
+            }]
         });
     }
 
     run(message, { text }) {
         const embed = new MessageEmbed()
-            .setColor('RANDOM')
+            .setTitle(`Just wanted to say...`)
+            .setColor('#888888')
             .setDescription(text)
-            .setTimestamp(new Date().toISOString())
+            .setTimestamp()
             .setFooter(
-                'Sent by ' + message.member.displayName,
+                `${message.member.displayName}, made me say it!`,
                 message.author.displayAvatarURL()
             );
-
-        if (announcementChannel != undefined) {
-            let announcedChannel = message.guild.channels.cache.find(
-                channel => channel.name == announcementChannel
-            );
-
-            if (message.guild.channels.cache.get(announcementChannel)) {
-                announcedChannel = message.guild.channels.cache.get(announcementChannel);
-            }
-
-            if (!announcedChannel) {
-                message.reply(':x: ' + announcementChannel + ' could not be found.');
-                return;
-            }
-
-            announcedChannel
-                .send(embed)
-                .catch(e => console.log(e));
-            return;
-        } else if (announcementChannel) {
-            message.channel
-                .send(embed)
-                .then(
-                    () => message.delete().catch(e => console.error(e))
-                )
-                .catch(e => console.error(e));
-            return;
-        }
+        message.channel
+            .send(embed)
+            .then(
+                () => message.delete().catch(e => console.error(e)) // nested promise
+            )
+            .catch(e => console.error(e));
+        return;
     }
 };
