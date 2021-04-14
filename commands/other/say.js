bb8a7be9bd9f7b777ac719e80ce8a9064986c442
Nimbi <Darkwater409@gmail.com>
2021-04-13 17:32:45 -0700

    Update say.js

--- commands/other/say.js
+++ commands/other/say.js
@@ -2,40 +2,66 @@ const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class SayCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'say',
-      aliases: ['make-me-say', 'print'],
-      memberName: 'say',
-      group: 'other',
-      clientPermissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
-      description: 'Make the bot say anything!',
-      args: [
-        {
-          key: 'text',
-          prompt: ':microphone2: What do you want the bot to say?',
-          type: 'string'
-        }
-      ]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'say',
+            aliases: ['make-me-say', 'print'],
+            memberName: 'say',
+            group: 'other',
+            userPermissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
+            clientPermissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
+            description: 'Make the bot say anything!',
+            args: [{
+                    key: 'announcementChannel',
+                    prompt: 'In which channel do you want the announcement to be sent?',
+                    type: 'string',
+                    default: message => message.channel.id
+                },
+                {
+                    key: 'text',
+                    prompt: ':microphone2: What do you want the bot to say?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
 
-  run(message, { text }) {
-    const embed = new MessageEmbed()
-      .setTitle(`Just wanted to say...`)
-      .setColor('#888888')
-      .setDescription(text)
-      .setTimestamp()
-      .setFooter(
-        `${message.member.displayName}, made me say it!`,
-        message.author.displayAvatarURL()
-      );
-    message.channel
-      .send(embed)
-      .then(
-        () => message.delete().catch(e => console.error(e)) // nested promise
-      )
-      .catch(e => console.error(e));
-    return;
-  }
+    run(message, { text }) {
+        const embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(text)
+            .setTimestamp(new Date().toISOString())
+            .setFooter(
+                'Sent by ' + message.member.displayName,
+                message.author.displayAvatarURL()
+            );
+
+        if (announcementChannel != undefined) {
+            let announcedChannel = message.guild.channels.cache.find(
+                channel => channel.name == announcementChannel
+            );
+
+            if (message.guild.channels.cache.get(announcementChannel)) {
+                announcedChannel = message.guild.channels.cache.get(announcementChannel);
+            }
+
+            if (!announcedChannel) {
+                message.reply(':x: ' + announcementChannel + ' could not be found.');
+                return;
+            }
+
+            announcedChannel
+                .send(embed)
+                .catch(e => console.log(e));
+            return;
+        } else if (announcementChannel) {
+            message.channel
+                .send(embed)
+                .then(
+                    () => message.delete().catch(e => console.error(e))
+                )
+                .catch(e => console.error(e));
+            return;
+        }
+    }
 };
