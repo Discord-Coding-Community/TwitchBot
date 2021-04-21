71418fd273ba0ab3e43123d7c1c1b4cc8a2183fb
Nimbi <Darkwater409@gmail.com>
2021-04-21 05:11:29 -0700

    Update commands

--- commands/dev/addgif.js
+++ commands/dev/addgif.js
@@ -12,18 +12,20 @@ module.exports = class AddGifCommand extends Command {
             group: 'dev',
             description: 'Adds a gif to the NSFW commands.',
             examples: [
-                '`' + config.prefix + 'addgif "https://hentai-gif.gif" "hentai"`'
+                '`' + config.prefix + 'addgif "https://blowjob-gif.gif" "1"`',
+                '`' + config.prefix + 'addgif "https://boobs-gif.gif" "2"`',
+                '`' + config.prefix + 'addgif "https://hentai-gif.gif" "3"`'
             ],
             guildOnly: false,
             isOwner: true,
             args: [{
-                    key: 'gifUrl',
+                    key: 'text',
                     prompt: 'Which gif would you like to add?',
                     type: 'string'
                 },
                 {
-                    key: 'txtFileName',
-                    prompt: 'What should I add it to?\n\n1. blowjoblinks\n2. boobslinks\n3. hentailinks\n\nPlease select a number.\n',
+                    key: 'choice',
+                    prompt: 'What should I add it to?\n\n1. Blowjob\n2. Boobs\n3. Hentai\n\nPlease select a number.\n',
                     type: 'string'
                 }
             ],
@@ -34,27 +36,41 @@ module.exports = class AddGifCommand extends Command {
         });
     }
 
-    async run(message, { gifUrl, txtFileName }) {
-
+    async run(message) {
+        if (message.author.bot) return;
         if (message.channel.nsfw) {
-            try {
-                let txtFileNumber = txtFileName;
 
-                fs.writeFile(`././resources/nsfw/${txtFileNumber}.txt`, 'utf8')
+
+            let text = message.content.split(" ");
+
+
+            if (choice.toLowerCase() == '1') choice = 'Blowjob';
+            if (choice.toLowerCase() == '2') choice = 'Boobs';
+            if (choice.toLowerCase() == '3') choice = 'Hentai';
+
+            if (choice.toLowerCase() == 'Blowjob') {
+                fs.writeFile(`././resources/nsfw/blowjoblinks.txt`, 'utf8')
                     .split('\n');
-                var embed = new MessageEmbed()
-                    .setTitle(this.client.user.username)
-                    .setDescription('New Gif added to ' + txtFileName)
-                    .setImage(`${gifUrl}`)
-                    .setThumbnail(this.client.user.displayAvatarURL())
-                    .setTimestamp(new Date().toISOString())
-                    .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
-                message.channel.send(embed);
-                return;
-            } catch (err) {
-                message.reply('```css\n [ERROR] Discord API Error:' + err.code + '(' + err.message + ')\n```');
-                return console.error(err);
+            } else if (choice.toLowerCase() == 'Boobs') {
+                fs.writeFile(`././resources/nsfw/boobslinks.txt`, 'utf8')
+                    .split('\n');
+            } else if (choice.toLowerCase() == 'Hentai') {
+                fs.writeFile(`././resources/nsfw/hentailinks.txt`, 'utf8')
+                    .split('\n');
             }
+
+            var embed = new MessageEmbed()
+                .setTitle('Gif Added')
+                .setDescription(`${text}`)
+                .addField('Category', choice, true)
+                .addImage(text)
+                .setThumbnail(this.client.user.displayAvatarURL())
+                .setTimestamp(new Date().toISOString())
+                .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
+            message.channel.send(embed).catch(err => {
+                message.channel.send('```css\n [ERROR] Discord API Error:' + err.code + '(' + err.message + ')\n```');
+                return console.error(err);
+            })
         }
     }
 };
