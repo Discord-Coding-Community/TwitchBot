ef50dbbdac9d20a4984e4c799edb0c0f377f3910
Nimbi <Darkwater409@gmail.com>
2021-05-04 03:28:25 -0700

    Update invite.js

--- commands/guild/invite.js
+++ commands/guild/invite.js
@@ -18,19 +18,19 @@ module.exports = class InviteCommand extends Command {
     }
 
     async run(message) {
-        const inviteURL = 'https://discord.com/api/oauth2/authorize?client_id=$' + this.client.user.id + '&permissions=8&scope=bot';
+        const inviteURL = 'https://discord.com/api/oauth2/authorize?client_id=$' + config.applicationID + '&permissions=8&scope=bot';
         const dsc = new Link.Client(config.dscAPI)
         dsc.fetchLink("discord").then((link) => {
             const embed = new MessageEmbed()
-            .setTitle(this.client.user.username)
-            .setDescription('A list of invites for ' + this.client.user.username)
-            .setColor('RANDOM')
-            .addField('TwitchBot', ':white_small_square: [Top.gg](' + inviteURL + ')' + '\n:white_small_square: [dsc.gg](' + config.dscInviteURL + ')', true)
-            .addField('Support Servers', ':white_small_square: [' + config.Support_Server_Name_1 + '](' + config.Support_Server_Invite_1 + ')\n:white_small_square: [' + config.Support_Server_Name_2 + '](' + config.Support_Server_Invite_2 + ')' , true)
-            .setThumbnail(this.client.user.displayAvatarURL())
-            .setTimestamp(new Date().toISOString())
-            .setFooter(this.client.user.username, this.client.user.displayAvatarURL());
-        message.channel.send(embed)
+                .setTitle(this.client.user.username)
+                .setDescription('A list of invites for ' + this.client.user.username)
+                .setColor('RANDOM')
+                .addField('TwitchBot', ':white_small_square: [Top.gg](' + inviteURL + ')' + '\n:white_small_square: [dsc.gg](' + config.dscInviteURL + ')', true)
+                .addField('Support Servers', ':white_small_square: [' + config.Support_Server_Name_1 + '](' + config.Support_Server_Invite_1 + ')\n:white_small_square: [' + config.Support_Server_Name_2 + '](' + config.Support_Server_Invite_2 + ')', true)
+                .setThumbnail(this.client.user.displayAvatarURL())
+                .setTimestamp(new Date().toISOString())
+                .setFooter(this.client.user.username, this.client.user.displayAvatarURL());
+            message.channel.send(embed)
         })
     }
 };
