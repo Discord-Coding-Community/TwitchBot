b53a6fe3a23744de450d52ac068d7b392624e5cb
Nimbi <Darkwater409@gmail.com>
2021-05-04 04:28:36 -0700

    Update shardError

--- index.js
+++ index.js
@@ -17,12 +17,4 @@ const manager = new ShardingManager('./bot.js', {
 
 manager.on('shardCreate', (shard) => console.log('Connecting to Shard: ' + shard.id));
 
-manager.on('shardError', error => {
-    try {} catch (e) {
-        let (e) = error;
-        if ((e) instanceof shardError) Error.captureStackTrace(e);
-        console.error('[ERROR] A web socket has encountered an error:', (e));
-    }
-});
-
 manager.spawn('auto');
