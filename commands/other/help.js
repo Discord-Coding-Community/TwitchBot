af4e85e38652ddfd9470a256f6c4760b6b301bf9
Joshua Lewis <josh@errite.org>
2021-05-24 00:43:43 -0700

    Fix Typo

--- commands/other/help.js
+++ commands/other/help.js
@@ -28,9 +28,7 @@ module.exports = class AvatarCommand extends Command {
 
     run(message, { user }) {
         const embed = new MessageEmbed()
-            .setTitle(this.client.name)
             .setDescription(`**__Base Commands__**\n:white_small_square: [Link](${Base_Commands})\n\n**__All Commands__**\n:white_small_square: [Link](${Full_Commands})\n\n**__Important__**\nDue to issues with Discord.js-Commando, Twitch Integration is becoming more and more unstable. As such we have implemented our own instance of **RedBot** for use with **ALL** of our stream commands.\n\nOur instance of **RedBot** is now enforeced for stream integration and comes with a ton of added features for everyone to enjoy.\n\n You can invite our instance of **RedBot** from the link below:\n:white_small_square: [${RedBot_Name}](${RedBot_Invite_URL})'n'n**__Support Discord__**\n:white_small_square: [${Support_Server_Name_1}](${Support_Server_Invite_1})\n[${Support_Server_Name_2}](${Support_Server_Invite_2})`)
-            .setImage(this.client.displayAvatarURL({ dynamic: true }))
             .setColor('RANDOM')
             .setTimestamp();
         message.embed(embed);
