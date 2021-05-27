5005a5dc88b3f4e927425495625b1da29be7baa0
Nimbi <Darkwater409@gmail.com>
2021-05-27 11:59:28 -0700

    Update help.js

--- commands/other/help.js
+++ commands/other/help.js
@@ -44,7 +44,7 @@ module.exports = class AvatarCommand extends Command {
         .addField("Utility Commands", `[Link](${Utility_Commands})`, true)
         .addField("Information", `The base version of TwitchBot carries a limited amount of functionality.\n\nTo get the full functionality you need to invite our instance of RedBot.`, false)
         .addField("RedBot Features", `:white_small_square: Twitch, YouTube, Smashcast, and Picarto stream integration\n:white_small_square: Autoplay support in the Music Player\n:white_small_square: Image Commands\n:white_small_square: Per Guild Configuration\n:white_small_square: much, much, more...`, false)
-        .addField("RedoBot Invite", `[Link](${RedBot_Invite_URL})`, true)
+        .addField("RedoBot Invite", `[Link](${RedBot_Invite_URL})`, false)
         .addField("Support Servers", `:white_small_square: [${Support_Server_Name_1}](${Support_Server_Invite_1})\n:white_small_square: [${Support_Server_Name_2}](${Support_Server_Invite_2})`, true)
         .addField("Privacy and API", `:white_small_square: [Privacy Policy](${Policy})\n:white_small_square: [API Usage](${API_Usage})`, true)
         .setThumbnail(this.client.user.displayAvatarURL())
