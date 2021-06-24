4591b36f6ebf9894efa2797c9a2af95855407885
Nimbi <Darkwater409@gmail.com>
2021-06-24 16:04:48 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,12 +54,8 @@ const client = new CommandoClient({
 client.registry
     .registerDefaultTypes()
     .registerGroups([
-        ['music', ':notes: **Music Command Group**:'],
-        ['gifs', ':film_frames: **Gif Command Group**:'],
-        ['nsfw', ':underage: **NSFW Command Group**:'],
         ['other', ':loud_sound: **Other Command Group**:'],
         ['guild', ':gear: **Guild Related Commands**:'],
-        ['speedrun', ':athletic_shoe: **Speedrun Related Commands**:']
     ])
     .registerDefaultGroups()
     .registerDefaultCommands({
