9fb1a047f43499c450411ef652ba24c8704594fc
Nimbi <Darkwater409@gmail.com>
2021-06-25 00:57:49 -0700

    Update invite.js

--- commands/twitchbot/invite.js
+++ commands/twitchbot/invite.js
@@ -22,7 +22,7 @@ module.exports = class AvatarCommand extends Command {
         const embed = new MessageEmbed()
             .setTitle(`${this.client.user.username} Instances`)
             .setThumbnail(this.client.user.displayAvatarURL())
-            .addField('TwitchBot', `**Invite**: [Link](https://discord.com/api/oauth2/authorize?client_id=779442792324661249&permissions=125952&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Ftoken&scope=bot)\n**Prefix**: \`t!\`\n**Description**:\nGateway Instance**`, true)
+            .addField('TwitchBot', `**Invite**: [Link](https://discord.com/api/oauth2/authorize?client_id=779442792324661249&permissions=125952&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Ftoken&scope=bot)\n**Prefix**: \`t!\`\n**Description**:\nPrimary Gateway Instance`, true)
             .addField('TwitchMusic', `**Invite**: [Link](https://discord.com/api/oauth2/authorize?client_id=846021695693127710&permissions=36961280&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n**Prefix**: \`m!\`\n**Description**:\nMusic and Audio Commands Instance`, true)
             .addField('TwitchUtils', `**Invite**: [Link](https://discord.com/api/oauth2/authorize?client_id=857768812332711966&permissions=2117598801&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n**Prefix**: \`u!\`\n**Description**:\nUtility and Fun Commands Instance`, true)
             .addField('TwitchGaurdian', `**Invite**: [Link](https://discord.com/api/oauth2/authorize?client_id=783958910536646658&permissions=536870646&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n**Prefix**: \`g!\`\n**Description**:\nAdministration and Moderation Commands Instance`, true)
