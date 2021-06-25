537157a826814ca5738873902d214af70d3dc543
Nimbi <Darkwater409@gmail.com>
2021-06-24 23:46:43 -0700

    Update help.js

--- commands/twitchbot/help.js
+++ commands/twitchbot/help.js
@@ -2,11 +2,6 @@ const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 const {
     prefix,
-    TwitchMusic_Invite_URL,
-    TwitchStreams_Invite_URL,
-    TwitchGaurdian_Invite_URL,
-    TwitchUtils_Invite_URL,
-    TwitchEconomy_Invite_URL,
     Support_Server_Name_1,
     Support_Server_Name_2,
     Support_Server_Invite_1,
@@ -30,7 +25,7 @@ module.exports = class AvatarCommand extends Command {
     async run(message) {
         const embed = new MessageEmbed()
             .setThumbnail('TwitchBot Help')
-            .setDescription(`**TwitchBot** uses a series of seperate bots to seperate it's features so that users will only have what they want.\n\nTo get the features you desire, simply invite your preferred instances from the links below:\n\n:white_small_square: [TwitchMusic](${TwitchMusic_Invite_URL})\n:white_small_square: [TwitchStreams](${TwitchStreams_Invite_URL})\n:white_small_square: [TwitchGuardian](${TwitchGaurdian_Invite_URL})\n:white_small_square: [TwitchUtils](${TwitchUtils_Invite_URL})\n:white_small_square: [TwitchEconomy](${TwitchEconomy_Invite_URL})\n\n:white_small_square: **Commands**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#commands)\n\n**__Support__**\n\n:white_small_square: [${Support_Server_Name_1}](${Support_Server_Invite_1})\n:white_small_square: [${Support_Server_Name_2}](${Support_Server_Invite_2})`)
+            .setDescription(`**TwitchBot** uses a series of seperate bots to seperate it's features so that users will only have what they want.\n\nTo get the features you desire, simply invite your preferred instances.\n\n:white_small_square: [Instances](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#instances)\n\n:white_small_square: **Commands**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#commands)\n\n**__Support__**\n\n:white_small_square: [${Support_Server_Name_1}](${Support_Server_Invite_1})\n:white_small_square: [${Support_Server_Name_2}](${Support_Server_Invite_2})`)
             .setThumbnail(this.client.user.displayAvatarURL())
             .setColor('RANDOM')
             .setTimestamp()
