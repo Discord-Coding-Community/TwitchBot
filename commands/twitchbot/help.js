d5047e9a6889844f002325ed8294405bd4f80bf8
Nimbi <Darkwater409@gmail.com>
2021-06-24 23:48:55 -0700

    Update help.js

--- commands/twitchbot/help.js
+++ commands/twitchbot/help.js
@@ -25,7 +25,7 @@ module.exports = class AvatarCommand extends Command {
     async run(message) {
         const embed = new MessageEmbed()
             .setThumbnail('TwitchBot Help')
-            .setDescription(`**TwitchBot** uses a series of seperate bots to seperate it's features so that users will only have what they want.\n\nTo get the features you desire, simply invite your preferred instances.\n\n:white_small_square: **Instances**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#instances)\n\n:white_small_square: **Commands**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#commands)\n\n**__Support__**\n\n:white_small_square: [${Support_Server_Name_1}](${Support_Server_Invite_1})\n:white_small_square: [${Support_Server_Name_2}](${Support_Server_Invite_2})`)
+            .setDescription(`**TwitchBot** uses a series of seperate bots to seperate it's features so that users will only have what they want.\n\nTo get the features you desire, simply invite your preferred instances.\n\n:white_small_square: **Instances**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#instances)\n:white_small_square: **Commands**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#commands)\n\n**__Support__**\n\n:white_small_square: [${Support_Server_Name_1}](${Support_Server_Invite_1})\n:white_small_square: [${Support_Server_Name_2}](${Support_Server_Invite_2})`)
             .setThumbnail(this.client.user.displayAvatarURL())
             .setColor('RANDOM')
             .setTimestamp()
