5a0e2ed01278641f9aea927101fb7192d68d8783
Nimbi <Darkwater409@gmail.com>
2021-06-25 00:04:09 -0700

    Update help.js

--- commands/twitchbot/help.js
+++ commands/twitchbot/help.js
@@ -20,7 +20,6 @@ module.exports = class AvatarCommand extends Command {
 
     async run(message) {
         const embed = new MessageEmbed()
-            .setThumbnail('https://i.imgur.com/RKPlGq5.jpg')
             .setDescription(`**TwitchBot** uses a series of seperate bots to seperate it's features so that users will only have what they want.\n\nTo get the features you desire, simply invite your preferred instances.\n\n:white_small_square: **Commands**: \`help\`, \`about\`, \`bot-status\`, \`ping\`\n:white_small_square: **Instances**: [Link](https://discord-coding-community.gitbook.io/discord-coding-community/twitchbot#instances)\n\n**__Support__**\n\n:white_small_square: [MountainT Development](https://discord.me/mtdev)\n:white_small_square: [Discord Coding Community](https://discord.me/discord-coding-community)`)
             .setThumbnail(this.client.user.displayAvatarURL())
             .setColor('RANDOM')
