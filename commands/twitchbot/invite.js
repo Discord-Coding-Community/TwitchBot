f070123c42cada979e4f1f6fbcd1abb051a63607
Nimbi <Darkwater409@gmail.com>
2021-06-25 00:39:47 -0700

    Update invite.js

--- commands/twitchbot/invite.js
+++ commands/twitchbot/invite.js
@@ -20,8 +20,13 @@ module.exports = class AvatarCommand extends Command {
 
     async run(message) {
         const embed = new MessageEmbed()
-            .setDescription(`**__TwitchBot Instances__**\n\n:white_small_square:**TwitchMusic**: [Link](https://discord.com/api/oauth2/authorize?client_id=846021695693127710&permissions=36961280&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n:white_small_square:**TwitchUtils**: [Link](https://discord.com/api/oauth2/authorize?client_id=857768812332711966&permissions=2117598801&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n:white_small_square:**TwitchGaurdian**: [Link](https://discord.com/api/oauth2/authorize?client_id=783958910536646658&permissions=536870646&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n:white_small_square:**TwitchEconomy**: [Link](https://discord.com/api/oauth2/authorize?client_id=857770441376399371&permissions=2118123249&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)\n:white_small_square:**TwitchStreams**: [Link](https://discord.com/api/oauth2/authorize?client_id=857756784709075004&permissions=574094928&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot)`)
+            .setTitle(`${this.client.user.username} Instances`)
             .setThumbnail(this.client.user.displayAvatarURL())
+            .addField(TwitchMusic, `[Invite](https://discord.com/api/oauth2/authorize?client_id=846021695693127710&permissions=36961280&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot) - Prefix: \`m!\``, false)
+            .addField(TwitchUtils, `[Invite](https://discord.com/api/oauth2/authorize?client_id=857768812332711966&permissions=2117598801&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot) - Prefix: \`u!\``, false)
+            .addField(TwitchGaurdian, `[Invite](https://discord.com/api/oauth2/authorize?client_id=783958910536646658&permissions=536870646&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot) - Prefix: \`g!\``, false)
+            .addField(TwitchEconomy, `[Invite](https://discord.com/api/oauth2/authorize?client_id=857770441376399371&permissions=2118123249&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot) - Prefix: \`e!\``, false)
+            .addField(TwitchStreams, `[Invite](https://discord.com/api/oauth2/authorize?client_id=857756784709075004&permissions=574094928&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize&scope=bot) - Prefix: \`s!\``, false)
             .setColor('RANDOM')
             .setTimestamp()
             .setFooter(this.client.user.username, this.client.user.displayAvatarURL())
